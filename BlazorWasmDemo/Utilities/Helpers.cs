ded81b202e13d43943d00dc896c9a4be0120a8a1
rameshkunwar <rameshkunwar2037@gmail.com>
2022-10-27 15:19:23 +0200

    deserialization issue fixed

---
+++ BlazorWasmDemo/Utilities/Helpers.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+
+namespace BlazorWasmDemo.Utilities
+{
+    public static class Helpers
+    {
+        public static async Task<T> ReadAsAsync<T>(this HttpContent content)
+        {
+            var data = await content.ReadAsStringAsync();
+            var option = new JsonSerializerOptions
+            {
+                DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull,
+                PropertyNamingPolicy = null
+            };
+
+            return JsonSerializer.Deserialize<T>(data, option);
+        }
+    }
+}
